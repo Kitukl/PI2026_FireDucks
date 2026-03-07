4c03418c3cc437c4e8e91de044743c983010310e
Олег Кіт <kitoleg167@gmail.com>
2026-03-07 11:44:36 +0200

    [Application] Init project

---
+++ Application/StudyHub.Domain/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace StudyHub.Domain;
+
+public class Class1
+{
+}
---
+++ Application/StudyHub.UnitTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace StudyHub.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
