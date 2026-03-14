d622176a8fa0e28a398db33974a6dddcca31b7fa
Ostap <palamarcukostap7@gmail.com>
2026-03-14 20:24:20 +0200

    Add TaskRepo

--- Application/StudyHub.Infrastructure/Repositories/TaskRepository.cs
+++ Application/StudyHub.Infrastructure/Repositories/TaskRepository.cs
@@ -1,8 +1,12 @@
-using StudyHub.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using StudyHub.Core.Tasks.Interfaces;
 
 namespace StudyHub.Infrastructure.Repositories;
 
-public class TaskRepository : ITaskRepository
+public class TaskRepository(SDbContext context) : ITaskRepository
 {
-    
+    public async Task<int> GetCountAsync()
+    {
+        return await context.Tasks.CountAsync();
+    }
 }
