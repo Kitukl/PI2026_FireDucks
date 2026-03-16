a0a7f5c6fd433c518de9d711035be072ee341d06
daeshiro <dasha05girich@gmail.com>
2026-03-16 03:26:14 +0200

    ReposUpdate

--- Application/StudyHub.Core/Tasks/Interfaces/ITaskRepository.cs
+++ Application/StudyHub.Core/Tasks/Interfaces/ITaskRepository.cs
@@ -2,7 +2,7 @@ using StudyHub.Domain.Entities;
 
 namespace StudyHub.Core.Tasks.Interfaces;
 
-public interface ItaskRepository
+public interface ITaskRepository
 {
     Task<Task?> GetTaskAsync(Guid Id);
     Task<List<Task>> GetTasksAsync();
