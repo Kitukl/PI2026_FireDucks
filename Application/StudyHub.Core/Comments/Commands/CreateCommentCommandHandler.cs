1923c6fb21d8aa72c696741d6236b81e061022bb
Олег Кіт <kitoleg167@gmail.com>
2026-03-23 00:20:47 +0200

    TaskHanlder

---
+++ Application/StudyHub.Core/Comments/Commands/CreateCommentCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using StudyHub.Core.Comments.Interfaces;
+using StudyHub.Domain.Entities;
+using Task = StudyHub.Domain.Entities.Task;
+
+namespace StudyHub.Core.Comments.Commands;
+
+public class CreateCommentCommand : IRequest<Comment>
+{
+    public string UserName { get; set; }
+    public string Description { get; set; }
+    public Task Task { get; set; }
+}
+
+public class CreateCommentCommandHandler : IRequestHandler<CreateCommentCommand, Comment>
+{
+    private readonly ICommentRepository _repository;
+
+    public CreateCommentCommandHandler(ICommentRepository repository)
+    {
+        _repository = repository;
+    }
+    public async Task<Comment> Handle(CreateCommentCommand request, CancellationToken cancellationToken)
+    {
+        return new Comment();
+    }
+}
---
+++ Application/StudyHub.Core/Tasks/Commands/UpdateTaskCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using StudyHub.Core.Tasks.Interfaces;
+using StudyHub.Domain.Entities;
+using StudyHub.Domain.Enums;
+
+namespace StudyHub.Core.Tasks.Commands;
+
+public class UpdateTaskCommand : IRequest<Guid>
+{
+    public Guid Id { get; set; }
+    public Status Status { get; set; }
+    public Subject Subject { get; set; }
+}
+
+public class UpdateTaskCommandHandler : IRequestHandler<UpdateTaskCommand, Guid>
+{
+    private readonly ITaskRepository _taskRepository;
+
+    public UpdateTaskCommandHandler(ITaskRepository taskRepository)
+    {
+        _taskRepository = taskRepository;
+    }
+    public async Task<Guid> Handle(UpdateTaskCommand request, CancellationToken cancellationToken)
+    {
+        var task = await _taskRepository.GetTaskAsync(request.Id) ?? throw new Exception("Task not found");
+        task.Status = request.Status;
+        task.Subject = request.Subject;
+        return await _taskRepository.UpdateTaskAsync(task);
+    }
+}
