e3e7994b99f2f19a27d4e585b1ee5eee78c5132e
Kitukl <150851717+Kitukl@users.noreply.github.com>
2026-04-19 16:55:02 +0300

    Refactored group controller (#50)

---
+++ Application/StudyHub.Core/Group/Commands/DeleteGroupCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+
+namespace StudyHub.Core.Group.Commands;
+
+public class DeleteGroupCommand : IRequest<bool>
+{
+    public Guid Id { get; set; }
+}
+
+public class DeleteGroupCommandHandler : IRequestHandler<DeleteGroupCommand, bool>
+{
+    private readonly IGroupRepository _groupRepository;
+
+    public DeleteGroupCommandHandler(IGroupRepository groupRepository)
+    {
+        _groupRepository = groupRepository;
+    }
+    public async Task<bool> Handle(DeleteGroupCommand request, CancellationToken cancellationToken)
+    {
+        return await _groupRepository.DeleteGroupAsync(request.Id);
+    }
+}
---
+++ Application/StudyHub.Core/Group/Queries/GetGroupQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using StudyHub.Core.DTOs;
+
+namespace StudyHub.Core.Group.Queries;
+
+public class GetGroupQuery : IRequest<GroupDto>
+{
+    public Guid Id { get; set; }
+}
+
+public class GetGroupQueryHandler : IRequestHandler<GetGroupQuery, GroupDto>
+{
+    private readonly IGroupRepository _groupRepository;
+
+    public GetGroupQueryHandler(IGroupRepository groupRepository)
+    {
+        _groupRepository = groupRepository;
+    }
+    public async Task<GroupDto> Handle(GetGroupQuery request, CancellationToken cancellationToken)
+    {
+        var group = await _groupRepository.GetGroupByIdAsync(request.Id);
+        return new GroupDto
+        {
+            Id = group.Id,
+            Name = group.Name
+        };
+    }
+}
