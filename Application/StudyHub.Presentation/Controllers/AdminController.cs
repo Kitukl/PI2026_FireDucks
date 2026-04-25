4822ddcf1b4bc6748707eb3649abfd219ba5ff1f
Taras Mazepa <67551103+kolbaser228@users.noreply.github.com>
2026-04-25 16:03:23 +0300

    FixTest

--- Application/StudyHub.Presentation/Controllers/AdminController.cs
+++ Application/StudyHub.Presentation/Controllers/AdminController.cs
@@ -286,7 +286,6 @@ public class AdminController(IMediator mediator) : Controller
     [HttpPost]
     public async Task<IActionResult> RemoveGroupSchedule(Guid groupId)
     {
-        dfgdfg
         await mediator.Send(new DeleteScheduleForGroupRequest(groupId));
         return RedirectToAction(nameof(Schedule));
     }
